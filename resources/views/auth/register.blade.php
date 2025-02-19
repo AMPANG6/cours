ecf585bee8a03922b95ba0c140991aa6d6fd3e0b
AMPANG6 <nearstore6@gmail.com>
2025-02-20 00:08:40 +0800

    ada eror dikit tadi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -111,6 +111,16 @@
                                 <div class="clearfix">
                                     <button class="btn btn-primary float-end">Submit</button>
                                 </div>
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
                             </form>
                         </div>
                     </div>
